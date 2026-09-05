72c0789e835b3c219be334605c55d07ffebc9eee
eugenius <kabatin21@gmail.com>
2026-09-05 17:27:39 +0200

    config upgrade for Docker

--- config/Database.php
+++ config/Database.php
@@ -1,19 +1,26 @@
 <?php
 
 class Database {
-    private $host = "localhost";
-    private $db_name = "healthylife";
-    private $username = "root";
-    private $password = "";
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
 
     public $conn;
+
+    public function __construct() {
+        $this->host = getenv('DB_HOST') ?: 'localhost';
+        $this->db_name = getenv('DB_NAME') ?: 'healthylife';
+        $this->username = getenv('DB_USER') ?: 'root';
+        $this->password = getenv('DB_PASSWORD') !== false ? getenv('DB_PASSWORD') : '';
+    }
 
     public function connect() {
         $this->conn = null;
 
         try {
             $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8mb4",
                 $this->username,
                 $this->password
             );
